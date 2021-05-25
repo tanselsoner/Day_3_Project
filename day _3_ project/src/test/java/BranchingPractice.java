77d543bbd28a610055d63a6cf79efd4ffa6928a4
tanselsoner <82914615+tanselsoner@users.noreply.github.com>
2021-05-26 05:24:35 +0600

    shared on github

--- day _3_ project/src/test/java/BranchingPractice.java
+++ day _3_ project/src/test/java/BranchingPractice.java
@@ -1,2 +1,10 @@
 public class BranchingPractice {
+
+    public static void main(String[] args) {
+    // created new project
+    //enabled version control
+    //added gitignore with.idea, target folder ant .iml file
+    //made initial commit by keeping track of all files
+    //shared project on github
+    }
 }
